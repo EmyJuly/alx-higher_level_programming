0d48b2e281a4768a668b89885999bb42efc4957e
root <farsimane00@gmail.com>
2024-01-07 14:26:14 -0800

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST
-request to the passed URL with the email as a parameter, and
-displays the body of the response
+Module to access to the GitHub API and uses the information
 """
-from urllib import request, parse
-import sys
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+
+def main(argv):
+    """
+    Function that list 10 commits (from the most recent to oldest)
+    of the repository.The first argument will be the repository name
+    and the second argument will be the owner name
+    """
+
+    def print_commits(i, commit_list):
+        """
+        List the commits, less than 10 commits
+        """
+        sha = commit_list[i].get('sha')
+        commit = commit_list[i].get('commit')
+        author = commit.get('author')
+        name = author.get('name')
+        print('{}: {}'.format(sha, name))
+
+    repo = argv[1]
+    owner = argv[2]
+    headers = {"Accept": "application/vnd.github.v3+json"}
+    response = requests.get('https://api.github.com/repos/' + owner +
+                            '/' + repo + '/commits', headers=headers)
+    commit_list = response.json()
+    size = len(commit_list)
+    if size < 10:
+        for i in range(0, size):
+            print_commits(i, commit_list)
+    else:
+        for i in range(0, 10):
+            print_commits(i, commit_list)
 
 
 if __name__ == "__main__":
-    values = {'email': sys.argv[2]}
-    data = parse.urlencode(values)
-    data = data.encode('ascii')
-    req = request.Request(sys.argv[1], data)
-    with request.urlopen(req) as response:
-        body = response.read()
-        print(body.decode('utf-8'))
+    main(argv)
